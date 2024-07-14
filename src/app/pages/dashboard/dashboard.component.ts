1c88accc906ea667749a5853309625f8f5e4c6a7
Diego Sequeira <jsequeirag@ucenfotec.ac.cr>
2024-07-14 14:00:41 -0600

    Commit for pull from development

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -9,11 +9,6 @@ import { LocationGMapsComponent } from '../../components/locationGMaps/locationG
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  // Import the Google Maps component
-  // Assuming the component is named GoogleMapsComponent
-  // Replace 'GoogleMapsComponent' with the actual name of your component
-  // Also, make sure to import the component at the top of this file
-  // import { GoogleMapsComponent } from './path/to/google-maps.component';
   gMapsComponent: LocationGMapsComponent;
   
   constructor() {

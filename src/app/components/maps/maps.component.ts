821e5c80c654298d1a7f2c463c04d38dd6667c6b
Diego Sequeira <jsequeirag@ucenfotec.ac.cr>
2024-07-22 20:52:02 -0600

    Google Maps Done

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-maps',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, GoogleMapsModule],
   templateUrl: './maps.component.html',
   styleUrl: './maps.component.scss'
 })
 export class MapsComponent {
-
+  
+  options: google.maps.MapOptions = {
+    center: { lat: -31, lng: 147 },
+    zoom: 4,
+  };
 }

dd4739048c9281af7124d71d706631013684451d
Ivan Wu Cen <iwuc@ucenfotec.ac.cr>
2024-07-26 16:27:31 -0600

    Update
    Issues fixed

--- src/app/pages/resetpassword/resetpassword.component.ts
+++ src/app/pages/resetpassword/resetpassword.component.ts
@@ -1,11 +1,10 @@
-import { UserService } from './../../services/user.service';
-import { IUser } from './../../interfaces/index';
 import { AuthService } from './../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, Output, ViewChild } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { ModalComponent } from "../../components/modal/modal.component";
+import { IUser } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-resetpassword',

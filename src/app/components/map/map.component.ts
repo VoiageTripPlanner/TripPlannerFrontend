ae811b552080ca0bbedf1a51a501f7fa12ebe1d7
Diego Sequeira <jsequeirag@ucenfotec.ac.cr>
2024-07-25 22:18:01 -0600

    Double Map Fixing

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-map',
+  selector: 'app-maps',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, GoogleMapsModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
-
+  
+  options: google.maps.MapOptions = {
+    center: { lat: -31, lng: 147 },
+    zoom: 4,
+  };
 }

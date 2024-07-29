91e11107757533410ae2ece6c757ba3a31de263b
Diego Sequeira <jsequeirag@ucenfotec.ac.cr>
2024-07-28 18:15:42 -0600

    Add Advance Markers

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMapsModule, MapInfoWindow, MapAdvancedMarker } from '@angular/google-maps';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,9 +10,38 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
-  
+  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
+  map!: google.maps.Map;
+  latitude: number = 0;
+  longitude: number = 0;
+  coordinates: string = this.latitude + ',' + this.longitude;
+  placeId: string = '';
+  address: string = '';
+
+  onMapClick(event: google.maps.MapMouseEvent) {
+    if (event.latLng) {
+      this.latitude = event.latLng.lat();
+      this.longitude = event.latLng.lng();
+    }
+    this.placeId = (event as any).placeId;
+    this.address = (event as any).address;
+  }
+
+  onMarkerClick(marker: MapAdvancedMarker) {
+    this.infoWindow.openAdvancedMarkerElement(marker.advancedMarker, marker.advancedMarker.title);
+  }
+
   options: google.maps.MapOptions = {
     center: { lat: -31, lng: 147 },
     zoom: 4,
   };
+
+  aLocations: any[] = [
+    { lat: 0, lng: 0 },
+  ];
+  bLocations: any[] = [
+    { lat: 0, lng: 0 },
+  ];
+
+
 }

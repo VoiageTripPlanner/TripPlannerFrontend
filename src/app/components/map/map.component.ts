bc0a5b79d7f31204d974eb4d1c486aa7fe5d3879
Diego Sequeira <jsequeirag@ucenfotec.ac.cr>
2024-08-02 12:41:52 -0600

    Single Dinamic Marker

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,70 +1,99 @@
-import { Component, ViewChild } from '@angular/core';
-import { GoogleMapsModule, MapInfoWindow, MapAdvancedMarker } from '@angular/google-maps';
+import { Component, OnInit } from '@angular/core';
+import { GoogleMapsModule, MapAdvancedMarker } from '@angular/google-maps';
 import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from '../loader/loader.component';
+import { environment } from '../../../environments/environment';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-maps',
   standalone: true,
-  imports: [LoaderComponent,RouterOutlet, GoogleMapsModule],
+  imports: [LoaderComponent,
+            CommonModule,
+            RouterOutlet, 
+            GoogleMapsModule,
+            MapAdvancedMarker,
+],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
-
-  // isLoading: boolean = false;
-  isLoading: boolean = true;
-
-  options: google.maps.MapOptions = {
-    center: { lat: -31, lng: 147 },
-    zoom: 4,
+export class MapComponent implements OnInit {
+  ngOnInit(): void {
+    if (!this.isValidApiKey()) {
+      throw new Error('Invalid API Key');
+    }else{
+      console.log('Valid');
+    }
+  }
+  markerOptions: google.maps.MarkerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral[] = [];
+  latitude: number = 9.748917;
+  longitude: number = -83.753428;
+  coordinates: { lat: number, lng: number } = {
+    lat: this.latitude ?? 0,
+    lng: this.longitude ?? 0
+  };
+  center: google.maps.LatLngLiteral = this.coordinates;
+  zoom: number = 8;
+  display: google.maps.LatLngLiteral = {
+    lat: this.latitude,
+    lng: this.longitude
   };
 
+  //Default coordinates for the map
+  moveMap(event: google.maps.MapMouseEvent) {
+    if (event.latLng) {
+      this.center = (event.latLng.toJSON());
+    }
+  }
+  //Update the display property with the new coordinates
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng) {
+      this.display = event.latLng.toJSON();
+    }
+  }
+
+  isLoading: boolean = true;
   onMapReady() {
     this.isLoading = false;
   }
 
-
-}
-
-  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
-  map!: google.maps.Map;
-  latitude: number = 0;
-  longitude: number = 0;
-  coordinates: string = this.latitude + ',' + this.longitude;
-  placeId: string = '';
-  address: string = '';
-
-  onMapClick(event: google.maps.MapMouseEvent) {
-    if (event.latLng) {
-      this.latitude = event.latLng.lat();
-      this.longitude = event.latLng.lng();
+  isValidApiKey(): boolean {
+    if(environment.maps_key) {
+      this.isLoading = false;
+      return true;
+    }else {
+      this.isLoading = true;
+      return false;
     }
-    this.placeId = (event as any).placeId;
-    this.address = (event as any).address;
   }
 
-  onMarkerClick(marker: MapAdvancedMarker) {
-    this.infoWindow.openAdvancedMarkerElement(marker.advancedMarker, marker.advancedMarker.title);
-  }
-
-  async initMap(): Promise<void> {
-    const { Map } = await google.maps.importLibrary("maps") as typeof google.maps;
-     this.map = new Map(document.getElementById("maps") as HTMLElement, {
-      center: { lat: this.latitude, lng: this.longitude },
-      zoom: 8,
-      mapId: `${environment.maps_id}`,
-    });
-  }
-
-  aLocations: any[] = [
-    { lat: 0, lng: 0 },
-  ];
-  bLocations: any[] = [
-    { lat: 0, lng: 0 },
-  ];
-
-  ngOnInit() {
-    this.initMap();
+  addOneMarker(event: google.maps.MapMouseEvent) {
+    if (event.latLng) {
+      if (this.markerPositions.length === 1) {
+        this.markerPositions.pop();
+      }
+      this.markerPositions.push(event.latLng.toJSON());
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ location: event.latLng }, (results, status) => {
+        if (status === "OK") {
+          if (results && results[0]) {
+            console.log(results[0]);
+            // Store the geocode object in a variable or use it as needed
+          } else {
+            console.log("No results found");
+          }
+        } else {
+          console.log("Geocoder failed due to: " + status);
+        }
+      });
+    }
   }
 }
+
+
+
+
+
+

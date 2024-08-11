16ff132f31ff9b1542f93db162bd6ffeab5ceca3
Kendall Duran <119521302+kendalljdv@users.noreply.github.com>
2024-08-11 16:38:11 -0600

    calendar design

--- src/app/components/calendar-planner/calendar-planner.component.ts
+++ src/app/components/calendar-planner/calendar-planner.component.ts
@@ -13,20 +13,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarPlannerComponent implements OnInit {
 
-  daysInMonth: Date[] = [];
 
-  ngOnInit() {
-    this.daysInMonth = this.getDaysInMonth(new Date().getFullYear(), new Date().getMonth());
+  weeks: number[][]     = [];
+  month: number         = new Date().getMonth();
+  year: number          = new Date().getFullYear();
+
+  monthNames: string[] = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+
+  ngOnInit(): void {
+    this.generateCalendar(this.month, this.year);
   }
 
-  getDaysInMonth(year: number, month: number): Date[] {
-    let date = new Date(year, month, 1);
-    const days = [];
-    while (date.getMonth() === month) {
-      days.push(new Date(date));
-      date.setDate(date.getDate() + 1);
+
+  generateCalendar(month: number, year: number) {
+    this.weeks              = []; 
+    const daysInMonth       = new Date(year, month + 1, 0).getDate();
+    let startDay            = new Date(year, month, 1).getDay();
+    startDay                = (startDay === 0) ? 6 : startDay - 1; 
+    const totalDays         = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+    let week: number[]      = [];
+
+    for (let i = 0; i < startDay; i++) {
+      week.push(0);  
     }
-    return days;
-  }
+
+    totalDays.forEach(day => {
+      week.push(day);
+      if (week.length === 7) {
+        this.weeks.push(week);
+        week = [];
+      }
+    });
+
+    if (week.length) {
+      while (week.length < 7) {
+        week.push(0); 
+      }
+      this.weeks.push(week);
+    }
+}
+
+
+  previousMonth() {
+    if (this.month === 0) {
+      this.month = 11;
+      this.year--;
+    } else {
+      this.month--;
+    }
+    this.generateCalendar(this.month, this.year);
+  };
+
+
+  nextMonth() {
+    if (this.month === 11) {
+      this.month = 0;
+      this.year++;
+    } else {
+      this.month++;
+    }
+    this.generateCalendar(this.month, this.year);
+  };
+
 
 }

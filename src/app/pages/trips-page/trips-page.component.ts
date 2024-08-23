621d51d89fe31a04f436e11a0daace5c0afd8b18
nefi.urena <nurenas@ucenfotec.ac.cr>
2024-08-23 14:18:51 -0600

    VBS22-162: Adding blank states and fixing bugs.

--- src/app/pages/trips-page/trips-page.component.ts
+++ src/app/pages/trips-page/trips-page.component.ts
@@ -4,8 +4,8 @@ import { ITrip } from '../../interfaces/trip.interface';
 import { IPagination } from '../../interfaces/pagination.interface';
 import { TripsTableComponent } from '../../components/trips-table/trips-table.component';
 import { PageEvent } from '@angular/material/paginator';
-import { LoaderComponent } from '../../components/loader/loader.component';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-trips-page',
@@ -15,21 +15,26 @@ import { Router } from '@angular/router';
   styleUrl: './trips-page.component.scss'
 })
 export class TripsPageComponent {
-  public tripPagination!: IPagination<ITrip>|null;
+  public tripPagination!: Signal<IPagination<ITrip>|null>;
   public isLoading: boolean = false;
   private tripService: TripService = inject(TripService);
   private router: Router = inject(Router);
+  private userService: any = inject(UserService);
+  private user: any;
 
   constructor() {
-    this.tripService.getTripsByUserAndPage(1, 0, 10);
+    this.tripPagination = this.tripService.tripsSig;
     effect(() => {
-      this.tripPagination = this.tripService.tripsSig();
+      this.user = this.userService.userSig();
       this.isLoading = false;
+      if (this.user.id) {
+        this.tripService.getTripsByUserAndPage(this.user.id, 0, 10);
+      }
     });
   }
 
   public changePage(page: PageEvent): void {
-    this.tripService.getTripsByUserAndPage(1, page.pageIndex, page.pageSize);
+    this.tripService.getTripsByUserAndPage(this.user.id, page.pageIndex, page.pageSize);
     this.isLoading = true;
   }
 

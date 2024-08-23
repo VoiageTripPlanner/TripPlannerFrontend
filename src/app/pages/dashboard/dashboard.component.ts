80ef0a4b6eecd63cafac2f5864358614375f5dbe
Diego Sequeira <jsequeirag@ucenfotec.ac.cr>
2024-08-23 16:20:12 -0600

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { CountriesHeatmapComponent } from '../../components/countries-heatmap/countries-heatmap.component';
 import { TopCountriesComponent } from '../../components/top-countries/top-countries.component';
